b106867cbea719c3a38979fff63fd84af898e585
sabbir268 <sabbir.hossain2668@gmail.com>
2020-11-15 14:44:13 +0600

    Color change for in kitchen

--- src/main.js
+++ src/main.js
@@ -56,62 +56,62 @@ import "prismjs/themes/prism-tomorrow.css";
 require("./assets/css/iconfont.css");
 
 Array.prototype.sum = function(prop) {
-  var total = 0;
-  for (var i = 0, _len = this.length; i < _len; i++) {
-    total += this[i][prop];
-  }
-  return total;
+    var total = 0;
+    for (var i = 0, _len = this.length; i < _len; i++) {
+        total += this[i][prop];
+    }
+    return total;
 };
 
 const plugin = {
-  install() {
-    Vue.prototype.showActionMessage = (type, msg) => {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        onOpen: toast => {
-          toast.addEventListener("mouseleave", Swal.resumeTimer);
-        }
-      });
+    install() {
+        Vue.prototype.showActionMessage = (type, msg) => {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                onOpen: toast => {
+                    toast.addEventListener("mouseleave", Swal.resumeTimer);
+                }
+            });
 
-      Toast.fire({
-        icon: type,
-        title: msg
-      });
-    };
+            Toast.fire({
+                icon: type,
+                title: msg
+            });
+        };
 
-    // checking error status
-    Vue.prototype.checkError = err => {
-      if (err.response.status === 401) {
-        localStorage.removeItem("token");
-        localStorage.removeItem("resturent_id");
-        // this.$router.push("/login").catch(() => {});
-        window.location.href = "/login";
-      }
-    };
+        // checking error status
+        Vue.prototype.checkError = err => {
+            if (err.response.status === 401) {
+                localStorage.removeItem("token");
+                localStorage.removeItem("resturent_id");
+                // this.$router.push("/login").catch(() => {});
+                window.location.href = "/login";
+            }
+        };
 
-    Vue.prototype.selectColor = status => {
-      switch (status) {
-        case "0_ORDER_INITIALIZED":
-          return "bl";
-        case "1_ORDER_PLACED":
-          return "ihosts";
-        case "2_ORDER_CONFIRMED":
-          return "grey-dark";
-        case "3_IN_TABLE":
-          return "pl";
-        case "4_PAID":
-          return "gn";
-        case "":
-          return "hard";
-        default:
-          return "";
-      }
-    };
-  }
+        Vue.prototype.selectColor = status => {
+            switch (status) {
+                case "0_ORDER_INITIALIZED":
+                    return "bl";
+                case "1_ORDER_PLACED":
+                    return "ihosts";
+                case "2_ORDER_CONFIRMED":
+                    return "ihostm";
+                case "3_IN_TABLE":
+                    return "pl";
+                case "4_PAID":
+                    return "gn";
+                case "":
+                    return "hard";
+                default:
+                    return "";
+            }
+        };
+    }
 };
 
 Vue.use(plugin);
@@ -119,7 +119,7 @@ Vue.use(plugin);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
