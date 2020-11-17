35c1d39da2d5db5bbcce253fc18a4a8779140aa0
lgope <ilakshmangope@gmail.com>
2020-11-17 13:57:08 +0600

    print issue solved

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -499,6 +499,7 @@ export default {
   data: () => ({
     time: "",
     resturent_id: localStorage.getItem("resturent_id"),
+    resturent: JSON.parse(localStorage.getItem("resturent")),
     orderActiveNow: "",
     tableScanned: "",
     userConfirmed: "",
@@ -513,6 +514,7 @@ export default {
     orderToVarify: [],
     orderToServed: [],
     selectedItemForVarify: [],
+    selectedItemForKitchen: [],
   }),
 
   methods: {
@@ -575,11 +577,16 @@ export default {
       if (this.selectedItemForVarify !== tempArr) {
         this.selectedItemForVarify = tempArr;
 
-        if (status === "user_confirmed")
+        if (status === "user_confirmed") {
           this.confirmProcess(
             order_id,
             "/restaurant_management/order/status/confirm/"
           );
+          let order = { ...this.ordersData };
+          let theorder = this.ordersData.find((order) => order.id === order_id);
+          console.log("order kitchen ", theorder);
+          this.printKitechRecit(theorder);
+        }
         if (status === "in_kitchen")
           this.confirmProcess(
             order_id,
@@ -601,6 +608,8 @@ export default {
               this.ordersData = this.ordersData.map((order) =>
                 order.id === order_id ? { ...res.data.data } : order
               );
+
+              this.selectedItemForVarify = "";
             }
           })
           .catch((err) => {
@@ -622,7 +631,7 @@ export default {
         .then((res) => {
           console.log("paymentResult ", res);
           // printRecipt(res.data.data);
-          printKitechRecit(order);
+          // printKitechRecit(order);
         })
         .catch((err) => {
           console.log("error paymentResult ", err.response);
@@ -1017,6 +1026,7 @@ export default {
     },
 
     printKitechRecit(order) {
+      console.log("test", order);
       const WinPrint = window.open(
         "",
         "",
