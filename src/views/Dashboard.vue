c3566920fd00d9a2fa5db5392ee743e9604f6e83
sabbir268 <sabbir.hossain2668@gmail.com>
2020-11-24 14:31:26 +0600

    Last commit

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -272,7 +272,6 @@
                 checkAllCanceled(order.ordered_items)
               "
             >
-              <!-- sabbir -->
               <vs-button
                 color="danger"
                 icon-pack="feather"
@@ -493,7 +492,6 @@
                 @click="verifyOrderItem(orderToServed.id, data[i].id)"
                 >Verify</span
               >
-              <!-- sabbir -->
             </vs-td>
           </vs-tr>
         </template>
@@ -533,6 +531,8 @@
         </vx-tooltip>
       </div>
     </vs-popup>
+
+    <img id="res_logo" :src="resturent.logo" alt="" style="display: none" />
   </div>
 </template>
 
@@ -627,11 +627,21 @@ export default {
     },
 
     checkAllServed(orderItemList) {
-      let totalLength = orderItemList.filter(
+      let init = orderItemList.filter(
+        (item) => item.status === "0_ORDER_INITIALIZED"
+      ).length;
+
+      let placed = orderItemList.filter(
+        (item) => item.status === "1_ORDER_PLACED"
+      ).length;
+
+      let confirmed = orderItemList.filter(
         (item) => item.status === "2_ORDER_CONFIRMED"
       ).length;
 
-      if (totalLength > 0) return false;
+      if (init > 0 || placed > 0 || confirmed > 0) {
+        return false;
+      }
 
       return true;
     },
@@ -736,7 +746,6 @@ export default {
             this.ordersData = this.ordersData.map((order) =>
               order.id === order_id ? { ...order, status: "" } : order
             );
-            printKitechRecit(order);
           }
         })
         .catch((err) => {
@@ -978,7 +987,7 @@ export default {
           break;
       }
     },
-
+    //sabbir
     printRecipt(order) {
       console.log(order);
       const WinPrint = window.open(
@@ -987,19 +996,42 @@ export default {
         "left=0,top=0,width=600,height=600,toolbar=0,scrollbars=0,status=0"
       );
 
-      // let itemDetail = "";
-      // let resLogo = document.querySelector("#res_logo").src;
+      let itemDetail = "";
+      let resLogo = document.querySelector("#res_logo").src;
 
-      // order.table[0].food.forEach((el) => {
+      order.table[0].food.forEach((el) => {
+        itemDetail += `<tr class="service">
+                        <td class="tableitem itemname">
+                            <p class="itemtext">${el.food_name}(<b>${
+          el.quantity
+        }</b>)</p>
+                        </td>
+                        <td class="tableitem">
+                            <p class="itemtext" style="text-align:center">${
+                              el.food_option.price
+                            }/-</p>
+                        </td>
+                        <td class="tableitem price">
+                            <p class="itemtext">${
+                              el.food_option.price * el.quantity
+                            }/-</p>
+                        </td>
+                    </tr>`;
+      });
+
+      // order.ordered_items.forEach((el) => {
       //   itemDetail += `<tr class="service">
-      //                   <td class="tableitem itemname">
-      //                       <p class="itemtext">${el.food.name}(<b>${el.quantity}</b>)</p>
-      //                   </td>
       //                   <td class="tableitem">
-      //                       <p class="itemtext" style="text-align:center">${el.food.price}/-</p>
+      //                       <p class="itemtext">${el.food_name}</p>
+      //                       ${
+      //                         el.food_option.option_type.name != "single_type"
+      //                           ? el.food_option.name
+      //                           : ""
+      //                       }
       //                   </td>
-      //                   <td class="tableitem price">
-      //                       <p class="itemtext">${el.price}/-</p>
+      //                   <td>-</td>
+      //                   <td class="tableitem qty">
+      //                       <p class="itemtext">${el.quantity}</p>
       //                   </td>
       //               </tr>`;
       // });
@@ -1011,144 +1043,91 @@ export default {
     <meta charset="UTF-8">
     <title>Invoice</title>
 
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-        }
-        
-        body {
-            margin: 0;
-            padding: 0;
-        }
-        
-        #invoice-POS {
-            box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5);
-            padding: 2mm;
-            margin: 0 auto;
-            width: 44mm;
-            background: #FFF;
-        }
-        
-        #invoice-POS ::selection {
-            background: #f31544;
-            color: #000;
-        }
-        
-        #invoice-POS ::moz-selection {
-            background: #f31544;
-            color: #000;
-        }
-        
-        #invoice-POS h1 {
-            font-size: 1.5em;
-            color: #222;
-        }
-        
-        #invoice-POS h2 {
-            font-size: .9em;
-        }
-        
-        #invoice-POS h3 {
-            font-size: 1.2em;
-            font-weight: 300;
-            line-height: 2em;
-        }
-        
-        #invoice-POS p {
-            font-size: .7em;
-            color: #000;
-            line-height: 1.2em;
-        }
-        /* #invoice-POS #top,
-        #invoice-POS #mid,
-        #invoice-POS #bot {
-            border-bottom: 1px solid #000;
-        } */
-        
-        #invoice-POS #top {
-            min-height: 77px;
-        }
-        
-        #invoice-POS #bot {
-            min-height: 50px;
-        }
-        
-        #invoice-POS #top .logo {
-            height: 60px;
-            width: 60px;
-        }
-        
-        
-        #invoice-POS .info {
-            display: block;
-            margin-left: 0;
-        }
-        
-        #invoice-POS .title {
-            float: right;
-        }
-        
-        #invoice-POS .title p {
-            text-align: right;
-        }
-        
-        #invoice-POS table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        
-        #invoice-POS .tabletitle {
-            font-size: .7em;
-            background: #EEE;
-        }
-        
-        #invoice-POS .service {
-            border-bottom: 1px solid #EEE;
-        }
-        
-        #invoice-POS .item {
-            width: 24mm;
-        }
-        
-        #invoice-POS .itemtext {
-            font-size: .7em;
-        }
-        
-        #invoice-POS #legalcopy {
-            margin-top: 5mm;
-        }
-        
-        .price>p,
-        .price>h2,
-        .payment>h2 {
-            float: right;
-            margin-right: 5px;
-        }
-        
-        .info {
-            padding: 5px 0px;
-        }
-        
-        .info>p {
-            text-align: center !important;
-        }
-        
-        .final {
-            border: 1px solid #000;
-            border-left: 0;
-            border-right: 0;
-        }
-        
-        .itemname>p {
-            margin-right: 5px;
-        }
-    </style>
+  
 </head>
 
 <body>
     <div id="invoice-POS">
         <center id="top">
+            <div class="logo">
+                <img src="${resLogo}" style="width: 100%;" alt="">
+            </div>
+
+            <div class="info">
+                <h2>${this.resturent.name}</h2>
+                <h2>Invoice</h2>
+            </div>
+        </center>
+        <div id="mid">
+            <div class="info">
+                <p>
+                    VAT Reg: ${this.resturent.tax_percentage}</br>
+                    Phone : 012938210983</br>
+                </p>
+            </div>
+        </div>
+        <div id="bot">
+            <center>
+                <h2>Order # ${order.id}</h2>
+                <h2>Table No: ${order.table_no}</h2>
+                <h2>Waiter: ${order.waiter.name}</h2>
+                <h2>Time: ${moment().format("DD/MM/Y, h:mma")}</h2>
+            </center>
+            <div id="table">
+                <table>
+                    <tr class="tabletitle">
+                        <td class="item">
+                            <h2>Item</h2>
+                        </td>
+                        <td class="Hours">
+                            <h2>U.Price</h2>
+                        </td>
+                        <td class="Rate price">
+                            <h2>T.Price</h2>
+                        </td>
+                    </tr>
+
+                    ${itemDetail}
+
+                    <tr class="tabletitle">
+                        <td class="Rate">
+                            <h2>Total</h2>
+                        </td>
+                        <td></td>
+                        <td class="payment">
+                            <h2>${order.price.total_price}/-</h2>
+                        </td>
+                    </tr>
+                    <tr class="tabletitle">
+                        <td class="Rate">
+                            <h2>Service Charge</h2>
+                        </td>
+                        <td></td>
+                        <td class="payment">
+                            <h2>${order.price.service_charge}/-</h2>
+                        </td>
+                    </tr>
+                    <tr class="tabletitle">
+                        <td class="Rate">
+                            <h2>VAT (15%)</h2>
+                        </td>
+                        <td></td>
+                        <td class="payment">
+                            <h2>${order.price.tax_amount}/-</h2>
+                        </td>
+                    </tr>
+                    <tr class="tabletitle final">
+                        <td class="Rate">
+                            <h2>Net Total:</h2>
+                        </td>
+                        <td></td>
+                        <td class="payment">
+                            <h2>${order.price.grand_total_price}/-
+                            </h2>
+                        </td>
+                    </tr>
+                </table>
+            </div>
             <div id="legalcopy">
                 <center>
                     <p class="legal"><strong> Powerd by @i-host <br> <small>www.i-host.com.bd</small></strong>
