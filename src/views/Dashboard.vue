50fdc8885d3ee42dacfd889eed4b4138f5c1ce65
sabbir268 <sabbir.hossain2668@gmail.com>
2020-12-13 13:10:07 +0600

    chart added

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -18,8 +18,8 @@
           class="mb-base"
           icon="CpuIcon"
           icon-right
-          statistic="86%"
-          statisticTitle="CPU Usage"
+          statistic="৳ 1,20,000"
+          statisticTitle="Total Sell current month"
         />
       </div>
 
@@ -29,8 +29,8 @@
           class="mb-base"
           icon="ServerIcon"
           icon-right
-          statistic="1.2gb"
-          statisticTitle="Memory Usage"
+          statistic="3400"
+          statisticTitle="Total Order current month"
           color="success"
         />
       </div>
@@ -41,8 +41,8 @@
           class="mb-base"
           icon="ActivityIcon"
           icon-right
-          statistic="0.1%"
-          statisticTitle="Downtime Ratio"
+          statistic="৳ 2,17,000"
+          statisticTitle="Total Sell last month"
           color="danger"
         />
       </div>
@@ -53,8 +53,8 @@
           class="mb-base"
           icon="AlertOctagonIcon"
           icon-right
-          statistic="13"
-          statisticTitle="Issues Found"
+          statistic="5300"
+          statisticTitle="Total Order last month"
           color="warning"
         />
       </div>
@@ -62,44 +62,13 @@
     <vs-divider />
 
     <div class="vx-row">
-      <div class="vx-col w-full md:w-2/3 mb-base">
-        <vx-card title="Revenue">
-          <template slot="actions">
-            <feather-icon
-              icon="SettingsIcon"
-              svgClasses="w-6 h-6 text-grey"
-            ></feather-icon>
-          </template>
-          <div slot="no-body" class="p-6 pb-0">
-            <div class="flex" v-if="revenueComparisonLine.analyticsData">
-              <div class="mr-6">
-                <p class="mb-1 font-semibold">This Month</p>
-                <p class="text-3xl text-success">
-                  <sup class="text-base mr-1">$</sup
-                  >{{
-                    revenueComparisonLine.analyticsData.thisMonth.toLocaleString()
-                  }}
-                </p>
-              </div>
-              <div>
-                <p class="mb-1 font-semibold">Last Month</p>
-                <p class="text-3xl">
-                  <sup class="text-base mr-1">$</sup
-                  >{{
-                    revenueComparisonLine.analyticsData.lastMonth.toLocaleString()
-                  }}
-                </p>
-              </div>
-            </div>
-            <vue-apex-charts
-              type="line"
-              height="266"
-              :options="analyticsData.revenueComparisonLine.chartOptions"
-              :series="revenueComparisonLine.series"
-            />
-          </div>
-        </vx-card>
-      </div>
+      <vx-card>
+        <chartjs-component-bar-chart
+          :height="150"
+          :data="data"
+          :options="options"
+        ></chartjs-component-bar-chart>
+      </vx-card>
     </div>
   </div>
 </template>
@@ -107,15 +76,16 @@
 <script>
 import StatisticsCardLine from "@/components/statistics-cards/StatisticsCardLine.vue";
 import VxTimeline from "@/components/timeline/VxTimeline";
-import VueApexCharts from "vue-apexcharts";
 import vSelect from "vue-select";
 import axios from "@/axios.js";
 import moment from "moment";
 
+import ChartjsComponentBarChart from "@/components/ChartjsComponentBarChart.vue";
+
 export default {
   components: {
     StatisticsCardLine,
-    VueApexCharts,
+    ChartjsComponentBarChart,
     VxTimeline,
     "v-select": vSelect,
   },
@@ -123,72 +93,35 @@ export default {
     time: "",
     resturent_id: localStorage.getItem("resturent_id"),
     resturent: JSON.parse(localStorage.getItem("resturent")),
-
-    revenueComparisonLine: {
-      analyticsData: { thisMonth: 86589, lastMonth: 73683 },
-      series: [
+    data: {
+      labels: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+      datasets: [
         {
-          name: "This Month",
-          data: [45000, 47000, 44800, 47500, 45500, 48000, 46500, 48600],
-        },
-        {
-          name: "Last Month",
-          data: [46000, 48000, 45500, 46600, 44500, 46500, 45000, 47000],
+          label: "Total Sale amount",
+          backgroundColor: [
+            "#3e95cd",
+            "#8e5ea2",
+            "#3cba9f",
+            "#e8c3b9",
+            "#c45850",
+          ],
+          data: [2478, 5267, 734, 784, 433],
         },
       ],
     },
-    analyticsData: {
-      revenueComparisonLine: {
-        chartOptions: {
-          chart: {
-            toolbar: { show: false },
-            dropShadow: {
-              enabled: true,
-              top: 5,
-              left: 0,
-              blur: 4,
-              opacity: 0.1,
-            },
-          },
-          stroke: { curve: "smooth", dashArray: [0, 8], width: [4, 2] },
-          grid: { borderColor: "#e7e7e7" },
-          legend: { show: false },
-          colors: ["#F97794", "#b8c2cc"],
-          fill: {
-            type: "gradient",
-            gradient: {
-              shade: "dark",
-              inverseColors: false,
-              gradientToColors: ["#7367F0", "#b8c2cc"],
-              shadeIntensity: 1,
-              type: "horizontal",
-              opacityFrom: 1,
-              opacityTo: 1,
-              stops: [0, 100, 100, 100],
-            },
-          },
-          markers: { size: 0, hover: { size: 5 } },
-          xaxis: {
-            labels: { style: { cssClass: "text-grey fill-current" } },
-            axisTicks: { show: false },
-            categories: ["01", "05", "09", "13", "17", "21", "26", "31"],
-            axisBorder: { show: false },
-            convertedCatToNumeric: true,
-          },
-          yaxis: {
-            tickAmount: 5,
-            labels: {
-              style: { cssClass: "text-grey fill-current" },
-              formatter: {
-                _custom: {
-                  type: "function",
-                  display: "<span>ƒ</span> formatter(val)",
-                },
-              },
-            },
-          },
-          tooltip: { x: { show: false } },
-        },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "This week sales",
       },
     },
   }),
