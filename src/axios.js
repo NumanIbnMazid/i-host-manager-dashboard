dfdc55058efe4f224cb25440f3d36d5d9d612da4
lgope <ilakshmangope@gmail.com>
2021-01-10 16:47:09 +0600

    changed base url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const baseURL = 'https://b9bf2244ef22.ngrok.io/api/'
-const baseURL = "http://production.i-host.com.bd/api/";
+const baseURL = "http://ihost.techrapples.com/api/";
 const authToken = localStorage.getItem("token");
 
 export default axios.create({
