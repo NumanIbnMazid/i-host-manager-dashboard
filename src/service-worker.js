9f8db6a975ce09cb370e1db18feb78458942dc79
NumanIbnMazid <numanibnmazid@gmail.com>
2021-05-24 15:54:41 +0600

    Working pwa with all routes

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,14 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.core.setCacheNameDetails({
+  prefix: "ihost-pwa"
+});
+
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-// workbox.precaching.suppressWarnings();
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute('/index.html');
 
 // install new service worker when ok, then reload page.
 self.addEventListener("message", msg => {
